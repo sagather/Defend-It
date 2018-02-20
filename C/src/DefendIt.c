47fb10ea27fe8d0c6872cbc7d7a7c802dfbdd8b7
Alysha <alysha.m.randall@gmail.com>
2018-02-20 09:30:55 -0800

    again

--- C/src/DefendIt.c
+++ C/src/DefendIt.c
@@ -5,7 +5,8 @@
 #include <unistd.h>
 #include <time.h>
 #include <regex.h>
-#include <crypt.h>
+//#include <crypt.h>
+
 
 char name[50];
 char pass[10];
@@ -14,20 +15,35 @@ void readName(char * name)
 {
    printf("Please enter your name: \n");
    fgets(name, 50, stdin);
-   strip(name);
+   //strip(name);
 }
 
 void readpass(char * pass)
 {
    printf("Please enter a password of length 10 and includes at least one upper case character, one lower case character, one digit, one punctuation mark: \n");
    fgets(pass, 10, stdin);
-   strip(pass);
+   //strip(pass);
 }
 
 int testPass(char * pass)
 {
-   re_t pattern = re_compile("^((?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[!@#$%]).{10})$");
-   return re_matchp(pattern, pass);
+    regex_t regex;
+    int reti;
+    reti = regcomp(&regex, "^((?=.\\d)(?=.[a-z])(?=.[A-Z])(?=.[!@#$%]).{10})$", 0);
+    if(reti)
+    {
+        -1;
+    }
+    reti = regexec(&regex, pass, 1, 1, 0);
+    if(!reti)
+    {
+        return 0;
+    }
+    else
+    {
+        printf("password does not meet requirement");
+        return 1;
+    }
 }
 
 int verify(char * pass)
@@ -46,9 +62,7 @@ int encrypt(char * pass)
 {
   unsigned long seed[2];
   char salt[] = "$1$........";
-  const char const alpha =
-    "./0123456789ABCDEFGHIJKLMNOPQRST"
-    "UVWXYZabcdefghijklmnopqrstuvwxyz";
+  const char const alpha = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   int i;
 
   // Generate a (not very) random seed? Rand() was not suggested?
@@ -57,7 +71,7 @@ int encrypt(char * pass)
   seed[1] = getpid() ^ (seed[0] >> 14 & 0x30000);
 
   for (i = 0; i < 10; i++)
-    salt[3+i] = alpha[(seed[i/5] >> (i%5)6) & 0x3f];
+    salt[3+i] = alpha[(seed[i/5] >> (i%5)*6) &0x3f];
 
 
   pass = crypt(pass, salt);
