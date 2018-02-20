af59c1bf4ab8589d0ccf29407ffcc1d18a3ea42e
sagather <samantha.agather@hotmail.com>
2018-02-20 15:58:29 -0800

    commented out things that were giving me errors because I'm on windows... I will be moving development to a linux environment in a while

--- C/src/DefendIt.c
+++ C/src/DefendIt.c
@@ -1,11 +1,11 @@
-#include <termios.h>
+//#include <termios.h>
 #include <stdio.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
-#include <regex.h>
-#include <crypt.h>
+//#include <regex.h>
+//#include <crypt.h>
 
 char name[50];
 char pass[10];
@@ -14,20 +14,20 @@ void readName(char * name)
 {
    printf("Please enter your name: \n");
    fgets(name, 50, stdin);
-   strip(name);
+   //strip(name);
 }
 
 void readpass(char * pass)
 {
    printf("Please enter a password of length 10 and includes at least one upper case character, one lower case character, one digit, one punctuation mark: \n");
    fgets(pass, 10, stdin);
-   strip(pass);
+   //strip(pass);
 }
 
 int testPass(char * pass)
 {
-   re_t pattern = re_compile("^((?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[!@#$%]).{10})$");
-   return re_matchp(pattern, pass);
+   //re_t pattern = re_compile("^((?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[!@#$%]).{10})$");
+   //return re_matchp(pattern, pass);
 }
 
 int verify(char * pass)
@@ -35,7 +35,7 @@ int verify(char * pass)
   char *result;
   int ok;
 
-  result = crypt(pass, encrypt(pass));
+  //result = crypt(pass, encrypt(pass));
   ok = strcmp (result, pass) == 0;
 
   puts(ok ? "Access granted." : "Access denied.");
@@ -57,10 +57,25 @@ int encrypt(char * pass)
   seed[1] = getpid() ^ (seed[0] >> 14 & 0x30000);
 
   for (i = 0; i < 10; i++)
-    salt[3+i] = alpha[(seed[i/5] >> (i%5)6) & 0x3f];
+//    salt[3+i] = alpha[(seed[i/5] >> (i%5)6) & 0x3f];
 
 
-  pass = crypt(pass, salt);
+  //pass = crypt(pass, salt);
   puts(pass);
   return 0;
+}
+
+int getInt(){
+
+    printf("Please enter an integer between ");
+
+}
+
+int main(int argc, const char* argv[]){
+
+
+
+
+
+
 }
